790048e5eff7aa8e0e9bde3032653ebb588ffd55
vboxuser <vboxuser@Core.myguest.virtualbox.org>
2023-04-26 18:53:07 -0400

    Add execute command

--- defer/src/switch.py
+++ defer/src/switch.py
@@ -1,4 +1,5 @@
 # required imports
+import os
 from core.emulator.coreemu import CoreEmu
 from core.emulator.data import IpPrefixes
 from core.emulator.enumerations import EventTypes
@@ -22,34 +23,32 @@ switch = session.add_node(SwitchNode, position=position)
 # create nodes
 position = Position(x=100, y=100)
 n1 = session.add_node(CoreNode, position=position)
-n1.set_model(mode="PC")
 position = Position(x=300, y=100)
 n2 = session.add_node(CoreNode, position=position)
-position = Position(x=200, y=100)
-n3 = session.add_node(CoreNode, position=position)
-position = Position(x=400, y=100)
-n4 = session.add_node(CoreNode, position=position)
 
 # link nodes to switch
 iface1 = ip_prefixes.create_iface(n1)
 session.add_link(n1.id, switch.id, iface1)
 print("n1:", iface1.ip4)
-iface1 = ip_prefixes.create_iface(n2)
-session.add_link(n2.id, switch.id, iface1)
-print("n2:", iface1.ip4)
-iface1 = ip_prefixes.create_iface(n3)
-session.add_link(n3.id, switch.id, iface1)
-print("n3:", iface1.ip4)
-iface1 = ip_prefixes.create_iface(n4)
-session.add_link(n4.id, switch.id, iface1)
-print("n4:", iface1.ip4)
+iface2 = ip_prefixes.create_iface(n2)
+session.add_link(n2.id, switch.id, iface2)
+print("n2:", iface2.ip4)
 
 # start session
 session.instantiate()
 
-# do whatever you like here
-input("press enter to shutdown")
+try:
+    # transfer files from local to nodes
+    session_name = session.name
+    nodes = [n1, n2]
 
-# stop session
-session.shutdown()
+    for node in nodes:
+        os.system(f"scp ~/Desktop/final-project-archimedes/defer/src/node.py root@{iface1.ip4}:/node.py")
+        print("done")
+        # execute script on node
+        node.cmd(f"python3 node.py")
+
+finally:
+    # stop session
+    session.shutdown()
 
